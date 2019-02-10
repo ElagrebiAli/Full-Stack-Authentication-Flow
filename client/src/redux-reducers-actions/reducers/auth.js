a9c4fa189edc8e3e7dc460a1a00debec539bf929
Elagrebi Ali <44264382+ElagrebiAli@users.noreply.github.com>
2019-02-10 13:49:23 +0100

    Update auth.js

--- client/src/redux-reducers-actions/reducers/auth.js
+++ client/src/redux-reducers-actions/reducers/auth.js
@@ -1,9 +1,10 @@
-import { AUTH_SIGN ,AUTH_ERROR ,AUTH_SIGN_OUT } from '../actions/types'
+import { AUTH_SIGN ,AUTH_ERROR_IN ,AUTH_SIGN_OUT,AUTH_ERROR_UP } from '../actions/types'
 
 const DEFAULT_STATE={
     isAuthenticated:false,
     token:'',
-    errorMessage:''
+    eerrorMessageSignIn:'',
+    errorMessageSignUp:''
 }
 
 export default (state=DEFAULT_STATE,action)=>{
@@ -11,8 +12,10 @@ export default (state=DEFAULT_STATE,action)=>{
         case AUTH_SIGN:  
         //override properties with ES9 (object Literals)
            return  {...state,token:action.payload,isAuthenticated:true,errorMessage:''}
-        case AUTH_ERROR:
-           return {...state,errorMessage:action.payload}
+        case AUTH_ERROR_IN:
+           return {...state,errorMessageSignIn:action.payload}
+        case AUTH_ERROR_UP:
+           return {...state,errorMessageSignUp:action.payload}
         case AUTH_SIGN_OUT:
            return {...state,isAuthenticated:false,token:'',email:'',errorMessage:''}
         default:
